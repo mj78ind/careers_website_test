8c17ef58f53a739510cca734d1e1e0a7e0e16e6c
Mayank Jaiswal <119934232+mj78ind@users.noreply.github.com>
2023-03-28 20:52:16 +0000

    Made the bootstrap-css work! Progressed to adding buttons in columns and making rows etc. Taking data from a list.

--- app.py
+++ app.py
@@ -2,9 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'Title':'Data Scientist',
+    'Location': 'Delhi, India',
+    'Salary': 'Rs 10,000,000'
+  },
+{
+    'id':2,
+    'Title':'Front End Engineer',
+    'Location': 'Bangalore, India'
+  },
+{
+    'id':3,
+    'Title':'Data Analyst',
+    'Location': 'New York, USA',
+    'Salary': 'USD 100,000'
+  }
+]
+
 @app.route("/")
 def hello_mayank():
-    return render_template('home.html')
+    return render_template('home.html', 
+                          jobs = JOBS,
+                          company_name = "Chukandar" )
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0',debug = True)
